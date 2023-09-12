0ac2ddae8a1b37f6b0ecf4d9bc4528c7f4f74380
shindetushar2004 <144783694+shindetushar2004@users.noreply.github.com>
2023-09-12 16:05:39 +0530

    Add files via upload
    java codes

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition
+{
+public static void main(String args[])
+{
+int a=75, b=45;
+int c;
+c=a+b;
+System.out.println(c);
+}
+}
---
+++ Arithmetic .java
@@ -0,0 +1,16 @@
+public class Arithmetic
+{
+public static void main(String args[])
+{
+int add,sub,mul,div;
+int a=20,b=10;
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+System.out.println("Addition="+add);
+System.out.println("Substraction="+sub);
+System.out.println("Multiplication="+mul);
+System.out.println("Division="+div);
+}
+}
